214f8fb52d8047d162e3e659761e4742bdfe2ba8
曹志国 <caozhiguo@daianla.cn>
2017-09-28 15:44:58 +0800

    这是初步的测试
    这是一个Tset工程，我专门为了测试GitHub来用的，希望可以成功

---
+++ HelloGitHub.java
@@ -0,0 +1,8 @@
+class HelloGitHub{
+	public static void main(String[] args) {
+        System.out.println("Hello World!");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println(args);
+        }
+	}
+}
